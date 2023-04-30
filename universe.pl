2987e75f4b8e45edfd7392ddf5999d84afcce750
Jaime-Rangel <jaime@MacBook-Air-de-Jaime.local>
2023-04-29 18:47:49 -0600

    step 2

--- universe.pl
+++ universe.pl
@@ -1,66 +1,69 @@
-% Facts
-norma_establecida(ley_general_de_salud, articulo(3, fraccion(xvii))).
-norma_establecida(ley_general_de_salud, articulo(134, fraccion(xiv))).
-norma_establecida(ley_general_de_salud, articulo(136, fraccion(ii))).
-norma_establecida(ley_general_de_salud, articulo(137)).
-norma_establecida(ley_general_de_salud, articulo(138)).
-norma_establecida(ley_general_de_salud, articulo(142)).
-norma_establecida(nom_017_ssa_2_2012, apartado('6.5')).
-norma_establecida(nom_017_ssa_2_2012, apartado('6.5.1')).
-norma_establecida(nom_017_ssa_2_2012, apartado('6.5.2')).
-norma_establecida(reglamento_sanitario_internacional, articulo(2)).
-norma_establecida(reglamento_sanitario_internacional, articulo(3)).
-norma_establecida(reglamento_sanitario_internacional, articulo(6)).
-norma_establecida(reglamento_sanitario_internacional, articulo(19)).
-norma_establecida(reglamento_sanitario_internacional, articulo(23)).
-norma_establecida(reglamento_sanitario_internacional, articulo(24)).
-norma_establecida(reglamento_sanitario_internacional, articulo(30)).
-norma_establecida(reglamento_sanitario_internacional, articulo(31)).
-norma_establecida(reglamento_sanitario_internacional, articulo(43)).
-
+%--------------------------------------------------Facts
 %Objectives for Covid suspects
-objetivo_general(objetivos_especificos,toma_muestra,sospechoso_COVID).
-objetivo_general(objetivos_especificos,manejo_muestra,sospechoso_COVID).
-objetivo_general(objetivos_especificos,envio_muestra,sospechoso_COVID).
+objetivos_especificos(toma_muestra,sospechoso_COVID).
+objetivos_especificos(manejo_muestra,sospechoso_COVID).
+objetivos_especificos(envio_muestra,sospechoso_COVID).
+
 %Objectives for covid behaviour
-objetivo_general(objetivos_especificos,monitoreo,comportamiento_COVID).
-objetivo_general(objetivos_especificos,alertamiento,comportamiento_COVID).
-objetivo_general(objetivos_especificos,difusion,comportamiento_COVID).
+objetivos_especificos(monitoreo,comportamiento_COVID).
+objetivos_especificos(alertamiento,comportamiento_COVID).
+objetivos_especificos(difusion,comportamiento_COVID).
 
-accion_vigilancia(atencion).
-accion_vigilancia(prevencion).
-accion_vigilancia(control).
+%Countries where covid is a warning
+pais_transmision_covid(viaje_estancia,[china,hong_kong,corea_del_sur,japon,italia,iran,singapur]).
 
-tipo_caso_covid(importado).
-tipo_caso_covid(autoctono).
+%What are the sintoms of covid
+enfermedad_covid(enfermedad,respiratoria,[aguda,leve,grave]).
+sintomas_covid(sintoma,respiratorio).
 
-presenta_enfermedad(enfermedad_respiratoria,[aguda,leve,grave]).
+contacto_covid(caso_covid,[confirmado,bajo_investigacion],[importado,autoctono]).
 
-pais_transmision(viaje_estancia,[china,hong_kong,corea_del_sur,japon,italia,iran,singapur]).
-
-contacto(caso,[confirmado,bajo_investigacion],[importado,autoctono]).
-% contacto(caso_bajo_investigacion_covid,[importado,autoctono]).
-
+diagnostico(covid_confirmado,inDre).
 objetivo_vigilancia_covid(prevenir_propagacion).
 
-caso_sospechoso(E1,E2,C1,C2,C3,P1,P2):-
-    presenta_enfermedad(E1,E2),contacto(C1,C2,C3);pais_transmision(P1,P2).
+personal_medico(medico,de_primer_contacto).
+personal_medico(personal_epidemiologia,coordinacion_jurisdiccion_sanitaria).
+
+unidad_salud(cubiculo,aislado,ventilado,puerta_cerrada).
+atencion_medica(primer_nivel).
+
+%Coordination jurisdiccion sanitaria
+estudio_epidemiologico(llenado_del_estudio).
+institucion_epidemiologica(toma_de_muestra).
+%estudio_epidemiologico(toma_de_muestra).
+usar(mascarilla_quirurgica_desechable).
+
+cubrise(boca).
+cubrirse(nariz).
+
+accion(tocer).
+accion(estornudar).
+
+%//////////////////////////////////////////////////////Rules
+
+caso_sospechoso(E1,E2,E3,C1,C2,C3,P1,P2):-
+    enfermedad_covid(E1,E2,E3),contacto_covid(C1,C2,C3);pais_transmision_covid(P1,P2).
+
+caso_confirmado(E1,E2,E3,C1,C2,C3,P1,P2,Y,Z) :-
+    caso_sospechoso(E1,E2,E3,C1,C2,C3,P1,P2),!,diagnostico(Y,Z).
+
+vigilancia_epidemiologica(X) :-
+    %Definicion operacional
+    write('caso sospechozo o caso confirmado'),
+    caso_sospechoso(_,_,_,_,_,_,_,_);caso_confirmado(_,_,_,_,_,_,_,_,_,_),objetivo_vigilancia_covid(X).
+
+%TODO with rules above
+% medidas_preventivas()
+
+% 1 Step for medias_preventivas
+interrogatorio_atencion_covid(X) :-
+    unidad_salud(C1,C2,C3,C4),personal_medico(M1,de_primer_contacto),caso_sospechoso(_,_,_,_,_,_,_,_).
+
+% 2 Step for medias_preventivas
+jurisdiccion_sanitaria(X,Y,Z) :-
+    atencion_medica(X),
+    estudio_epidemiologico(Y),
+    institucion_epidemiologica(Z).
 
 %definicion_operacional
 %--------------------------------Page 11---------------------------------
-
-%First Part
-% objetivos_especificos(toma_muestra, sospechoso_COVID).
-% objetivos_especificos(manejo_muestra, sospechoso_COVID).
-% objetivos_especificos(envio_muestra, sospechoso_COVID).
-%Second Part
-% objetivos_especificos(monitoreo, sospechoso).
-% objetivos_especificos(alertamiento, sospechoso).
-% objetivos_especificos(difusion, sospechoso).
-
-% Rules
-norma_sustentada(Texto, Norma) :- norma_establecida(Norma, _), sub_string(Texto, _, _, _, Norma).
-
-incumplimiento_descrito(Texto) :- sub_string(Texto, _, _, _, 'incumplimiento de las acciones de vigilancia epidemiológica descritas').
-
-motivo_de_reporte(Texto, Secretaria, Competencia) :- incumplimiento_descrito(Texto), sub_string(Texto, _, _, _, 'persistencia'), Secretaria = 'Secretaría de Salud', Competencia = 'instancia jurídica de su competencia'.
