e06c164972106acb9c409eaac68385930e806a91
Jaime-Rangel <jaime@MacBook-Air-de-Jaime.local>
2023-04-30 17:28:00 -0600

    more rules

--- universe.pl
+++ universe.pl
@@ -26,6 +26,8 @@ personal_medico(medico,de_primer_contacto).
 personal_medico(personal_epidemiologia,coordinacion_jurisdiccion_sanitaria).
 
 unidad_salud(cubiculo,aislado,ventilado,puerta_cerrada).
+unidad_medica(publica).
+unidad_medica(privada).
 atencion_medica(primer_nivel).
 
 %Coordination jurisdiccion sanitaria
@@ -35,11 +37,42 @@ institucion_epidemiologica(toma_de_muestra).
 usar(mascarilla_quirurgica_desechable,tolera).
 usar(papel_higienico,no_tolera).
 
+%Cuadro 1 Tarjeta Roja
+usar(guantes,tarjeta_roja).
+usar(mascarilla_proteccion_facial,tarjeta_roja).
+usar(bata_impermeable,tarjeta_roja).
+usar(contenedor_rigido,tarjeta_roja).
+usar(agua,tarjeta_roja).
+usar(jabon,tarjeta_roja).
+usar(alcohol_gel_70_porciento,tarjeta_roja).
+
+%Tarjeta Verde
+usar(alcohol_etilico_70_porciento,tarjeta_verde).
+
+%Tarjeta Azul
+usar(respirador_N95,tarjeta_azul).
+
+%Cuadro 2 Tarjeta Color Verde
+medidas_gotas(distancia,1_metro).
+medidas_gotas(puerta_cerrada,atencion).
+medidas_gotas(visitante,reportarse,enfermeras).
+medidas_gotas(equipo_medico,desechable).
+medidas_gotas(equipo_medico,personalizado).
+medidas_gotas(compartir_equipo_medico,limpiar,desinfectar).
+medidas_gotas(habitacion_individual).
+medidas_gotas(transportar_paciente).
+medidas_gotas(notificar_area).
+
+%Cuadro 2 Tarjeta Color Azul
+medias_aerosoles()
+
 cubrise(boca).
 cubrise(nariz).
 
 accion(tocer).
 accion(estornudar).
+accion(limpiar,tarjeta_verde).
+accion(desinfectar,tarjeta_verde).
 
 %//////////////////////////////////////////////////////Rules
 
@@ -70,7 +103,14 @@ higiene_respiratoria(X,Y) :-
     usar(X,no_tolera),
     accion(Y).
 
-% 2 Step for medias_preventivas for breath synthoms
+% 2 Step for medias_preventivas without synthoms
+jurisdiccion_sanitaria(X,Y,Z,W) :-
+    atencion_medica(X),
+    estudio_epidemiologico(Y),
+    institucion_epidemiologica(Z),
+    personal_medico(W,coordinacion_jurisdiccion_sanitaria).
+
+% 3 Step for medias_preventivas for breath synthoms
 jurisdiccion_sanitaria_sintomas(X,Y,Z,T1,T2,T3) :-
     atencion_medica(X),
     estudio_epidemiologico(Y),
