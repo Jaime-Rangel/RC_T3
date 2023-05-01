361d7744026e30f6dbbb970bb479ef2a091026b3
Jaime-Rangel <jaime@MacBook-Air-de-Jaime.local>
2023-05-01 00:05:31 -0600

    part 1 first table

--- universe.pl
+++ universe.pl
@@ -34,37 +34,31 @@ atencion_medica(primer_nivel).
 estudio_epidemiologico(llenado_del_estudio).
 institucion_epidemiologica(toma_de_muestra).
 
-usar(mascarilla_quirurgica_desechable,tolera).
-usar(papel_higienico,no_tolera).
-
-%Cuadro 1 Tarjeta Roja
-usar(guantes,tarjeta_roja).
-usar(mascarilla_proteccion_facial,tarjeta_roja).
-usar(bata_impermeable,tarjeta_roja).
-usar(contenedor_rigido,tarjeta_roja).
-usar(agua,jabon,lavado,tarjeta_roja).
-% usar(jabon,lavado,tarjeta_roja).
-usar(alcohol_gel_70_porciento,higiene,tarjeta_roja).
-
-%Tarjeta Verde
-usar(alcohol_etilico_70_porciento,tarjeta_verde).
-
-%Tarjeta Azul
-usar(respirador_N95,tarjeta_azul).
+usar_mascarilla_tolera(mascarilla_quirurgica_desechable,tolera).
+usar_mascarilla_tolera(papel_higienico,no_tolera).
+usar_guantes(guantes,tarjeta_roja).
+usar_mascarillla(mascarilla_proteccion_facial,salpicadura,tarjeta_roja).
+usar_bata(bata_impermeable,salpicadura,contaminacion,tarjeta_roja).
+usar_agua(agua,jabon,lavado,tarjeta_roja).
+usar_alcohol_gel(alcohol_gel_70_porciento,higiene,tarjeta_roja).
+usar_bolsa(bolsa_plastico,rojo,contaminacion_ropa,tarjeta_roja).
+usar_alcohol_etilico(alcohol_etilico_70_porciento,tarjeta_verde).
+usar_respirador(respirador_N95,tarjeta_azul).
+usar_contenedor(contenedor_rigido,desecho,contaminacion,tarjeta_roja).
 
 %Cuadro 2 Tarjeta Color Verde
-medidas_gotas(distancia,1_metro).
-medidas_gotas(puerta_cerrada,atencion).
-medidas_gotas(visitante,reportarse,enfermeras).
-medidas_gotas(equipo_medico,desechable).
-medidas_gotas(equipo_medico,personalizado).
-medidas_gotas(compartir_equipo_medico,limpiar,desinfectar).
-medidas_gotas(habitacion_individual).
-medidas_gotas(transportar_paciente).
-medidas_gotas(notificar_area).
+% medidas_gotas(distancia,metro_1).
+% medidas_gotas(puerta_cerrada,atencion).
+% medidas_gotas(visitante,reportarse,enfermeras).
+% medidas_gotas(equipo_medico,desechable).
+% medidas_gotas(equipo_medico,personalizado).
+% medidas_gotas(compartir_equipo_medico,limpiar,desinfectar).
+% medidas_gotas(habitacion_individual).
+% medidas_gotas(transportar_paciente).
+% medidas_gotas(notificar_area).
 
 %Cuadro 2 Tarjeta Color Azul
-medias_aerosoles(procedimiento,[aspiraciones,intubaciones,broncoscopias,reanimacion_cardiopulmonar]).
+medidas_aerosoles(procedimiento,[aspiraciones,intubaciones,broncoscopias,reanimacion_cardiopulmonar]).
 
 cubrise(boca).
 cubrise(nariz).
@@ -75,12 +69,22 @@ accion(limpiar,tarjeta_verde).
 accion(desinfectar,tarjeta_verde).
 accion(lavado,manos,tarjeta_roja).
 accion(higiene,manos,tarjeta_roja).
+accion(ponerse,guantes,tarjeta_roja).
 
-contacto(sangre).
-contacto(secreciones).
-contacto(articulo_contaminado).
-contacto(membranas_mucosas).
-contacto(heridas_piel).
+contacto(sangre,liquido).
+contacto(secreciones,liquido).
+contacto(articulo_contaminado,objeto).
+contacto(membranas_mucosas,objeto).
+contacto(heridas_piel,objeto).
+
+riesgo_salpicadura_en(salpicadura,ojos).
+riesgo_salpicadura_en(salpicadura,cara).
+
+contaminacion_de(contaminacion,piel).
+contaminacion_de(contaminacion,ropa).
+
+desechos_de(agujas).
+desechos_de(material_punzocortante).
 
 %//////////////////////////////////////////////////////Rules
 
@@ -96,16 +100,16 @@ vigilancia_epidemiologica(X) :-
     caso_sospechoso(_,_,_,_,_,_,_,_);caso_confirmado(_,_,_,_,_,_,_,_,_,_),objetivo_vigilancia_covid(X).
 
 % 1 Step for medias_preventivas
-interrogatorio_atencion_covid(X) :-
-    unidad_salud(C1,C2,C3,C4),personal_medico(M1,de_primer_contacto),caso_sospechoso(_,_,_,_,_,_,_,_).
+interrogatorio_atencion_covid(M1) :-
+    unidad_salud(_,_,_,_),personal_medico(M1,de_primer_contacto),caso_sospechoso(_,_,_,_,_,_,_,_).
 
 proveer_mascarilla_quirurjica(X):-
     sintomas_covid(sintoma,respiratorio),
-    usar(X,tolera).
+    usar_mascarilla_tolera(X,tolera).
 
 higiene_respiratoria(X,Y) :-
     sintomas_covid(sintoma,respiratorio),
-    usar(X,no_tolera),
+    usar_mascarilla_tolera(X,no_tolera),
     accion(Y).
 
 % 2 Step for medias_preventivas without synthoms
@@ -127,17 +131,41 @@ jurisdiccion_sanitaria_sintomas(X,Y,Z,T1,T2,T3) :-
 %TODO with rules below
 higiene_manos(X,Y,W,Z) :-
     write('lavado manos agua y jabon'),
-    (accion(X,Y,tarjeta_roja),usar(Z,W,X,tarjeta_roja),X \== higiene);
+    (accion(X,Y,tarjeta_roja),usar_agua(Z,W,X,tarjeta_roja),X \== higiene);
     write('o higiene manos y alcohol'),
-    (accion(X,Y,tarjeta_roja),usar(Z,X,tarjeta_roja),X == higiene).
+    (accion(X,Y,tarjeta_roja),usar_alcohol_gel(Z,X,tarjeta_roja),X == higiene).
 
-uso_guantes(X) :-
+guantes(X,Y) :-
     higiene_manos(_,_,_,_),
-    write('con contacto:')
-    contacto(X).
+    contacto(X,_),
+    usar_guantes(Y,_).
+
+salpicadura_liquidos(X,Y,Z) :-
+    contacto(X,liquido),
+    write('Salpicadura de liquido:'),
+    riesgo_salpicadura_en(Y,Z).
+
+mascarilla(X,Y) :-
+    salpicadura_liquidos(_,Y,_),
+    !, %not all cases
+    usar_mascarillla(X,Y,tarjeta_roja).
+
+bata(X,Y,Z,B) :-
+    contaminacion_de(X,Y),
+    salpicadura_liquidos(_,Z,_),
+    !,%not all cases
+    usar_bata(B,Z,X,tarjeta_roja).
+
+transporte_ropa(X,K,Y,W) :-
+    contaminacion_de(X,K),K == ropa,
+    usar_bolsa(Y,W,contaminacion_ropa,_).
+
+contenedor(X,Y,Z) :-
+    usar_contenedor(Y,Z,_,_),
+    desechos_de(X).
 
 % 4 Step for gota y contacto
-personal_primer_contacto(X) :-
+personal_primer_contacto() :-
     unidad_medica(publica),unidad_medica(privada),
     caso_sospechoso(_,_,_,_,_,_,_,_).
 
